5550ebd20a50f44cf35d7de763be5cbd1e8ee539
kyles7 <ksonandres@yahoo.com>
2022-10-14 19:02:20 -0400

    Implement get_vals function, Add example use

---
+++ controls/example_extract_geometry.py
@@ -0,0 +1,19 @@
+import extract_geometry
+design_element_values = {
+    'rho': None,
+    'S_ref': None, #m^2
+    'b': None, #m
+    'Cl_max': None, #stall occurs at about 20 deg AoA, at which point our airfoil produces this Cl
+    'fuse_radius': None, #m
+    'av_quarter_chord': None, #half of half the span
+    'g': None, #m/s
+    'plane_mass': None,#kg
+    'wing_mass': None, #kg
+    'fuse_mass': None,
+    'I_fuse': None,
+    'I_wing': None,
+    'I_plane': None,
+    'Cd_tail_roll': None
+}
+data = extract_geometry.get_values(design_element_values)
+print(data)
--- controls/extract_geometry.py
+++ controls/extract_geometry.py
@@ -2,19 +2,25 @@
 
 from __future__ import print_function
 import gspread
-SAMPLE_SPREADSHEET_ID = '1s5P5vXLdzXZgH1rN1ZvnTEgL87FyuiPga2gGJ-t785g'
+import pandas as pd
+SPREADSHEET_ID = '1s5P5vXLdzXZgH1rN1ZvnTEgL87FyuiPga2gGJ-t785g'
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
+def get_values(design_element_values):
+
     """
+    design_element_values is a dictionary with all of the geometry variables you want to extract from the spreadsheet. 
+    Map variable names to None to start (or a manual value). See example_extract_geometry for an example use case 
+    """""
     client = gspread.service_account('credentials_service.json')
-    sheet = client.open_by_key(SAMPLE_SPREADSHEET_ID)
-    configs_sheet = sheet.worksheet('Configurations')
+    sheet = client.open_by_key(SPREADSHEET_ID)
+    configs_sheet = sheet.worksheet('Controls Script Values')
     all_vals = configs_sheet.get_all_values()
+    df = pd.DataFrame(all_vals, columns=['Design Element', 'Variable Name', 'Value', 'Source'])
 
-    print(all_vals)
+    for key in design_element_values.keys():
+        if key in df['Variable Name'].values:
+            design_element_values[key] = float(df[df['Variable Name'] == key]['Value'])
+        else:
+            print(key, 'not in Master Value Sheet')
 
-
-if __name__ == '__main__':
-    main()
+    return design_element_values
