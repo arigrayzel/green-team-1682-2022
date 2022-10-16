b1e939db4c78d6542a73b0b6e0caa1d6d0f1c33c
Ari Grayzel <arigrayzel@gmail.com>
2022-10-16 09:04:30 -0400

    Fix constraints to get working control optimization

--- controls/rigid_dynamics.py
+++ controls/rigid_dynamics.py
@@ -3,6 +3,7 @@ import aerosandbox.numpy as np
 from aerosandbox.tools import units as u
 import create_mass_props
 import create_plane
+import matplotlib.pyplot as plt
 
 #import airplane?
 airplane = create_plane.create_plane()
@@ -25,7 +26,7 @@ dyn = asb.DynamicsRigidBody3DBodyEuler(
     x_e = opti.variable(init_guess=1000*np.linspace(0,1,N), n_vars=N),
     y_e = opti.variable(init_guess=0*np.linspace(0,1,N), n_vars=N),
     z_e = opti.variable(init_guess=-1000*np.ones(N), n_vars=N),
-    u_b = opti.variable(init_guess= 30*np.ones(N), n_vars=N, upper_bound=0),
+    u_b = opti.variable(init_guess= 30*np.ones(N), n_vars=N),
     v_b = opti.variable(init_guess= 0*np.ones(N), n_vars=N),
     w_b = opti.variable(init_guess= 0*np.ones(N), n_vars=N),
     phi = opti.variable(init_guess= np.zeros(N), n_vars=N),
@@ -58,7 +59,7 @@ opti.subject_to([
     dyn.phi[-1] > 2*np.pi-eps,
     #dyn.theta[-1] == 0,
     #dyn.psi[-1] == 0,
-    #dyn.p[-1] == 0,
+    dyn.p[-1] == 0,
     #dyn.q[-1] == 0,
     #dyn.r[-1] == 0
     ])
@@ -84,7 +85,7 @@ dyn.add_force(
 )
 
 #roll moment
-roll = opti.variable(init_guess=np.concatenate((10000*np.ones(N//2), -10000*np.ones(N//2))), n_vars=N, lower_bound=-10000, upper_bound=10000)
+roll = opti.variable(init_guess=np.concatenate((1000*np.ones(N//2), -1000*np.ones(N//2))), n_vars=N, lower_bound=-1000, upper_bound=1000)
 #pitch moment
 #pitch = opti.variable(init_guess=np.zeros(N), n_vars=N, lower_bound=-1000, upper_bound=1000)
 #yaw moment
@@ -100,8 +101,26 @@ dyn.add_moment(
 dyn.constrain_derivatives(opti, time)
 opti.minimize(final_time)
 sol = opti.solve()
-try:
-    dyn.substitute_solution(sol)
-except:
-    print(opti.debug.value)
-print("done!")
+dyn.substitute_solution(sol)
+
+fig, ax = plt.subplots(3,1)
+ax[0].plot(sol.value(time), sol.value(dyn.phi))
+ax[0].set_xlabel(r"Time(s)")
+ax[0].set_ylabel(r"Angle(rad)")
+
+ax[1].plot(sol.value(time), sol.value(dyn.p))
+ax[1].set_xlabel(r"Time(s)")
+ax[1].set_ylabel(r"Angular velocity(rad/s)")
+
+ax[2].plot(sol.value(time), sol.value(roll))
+ax[2].set_xlabel(r"Time(s)")
+ax[2].set_ylabel(r"Roll moment")
+
+#plt.show()
+
+plotter = dyn.draw(
+        vehicle_model=airplane,
+        scale_vehicle_model=0.25,
+        show=False
+        )
+plotter.show()
