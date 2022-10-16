545b90a92ee7c3c1ec1620fffde401353326b509
Ari Grayzel <arigrayzel@gmail.com>
2022-10-15 22:57:21 -0400

    Play with opti params to try to get convergence

--- controls/rigid_dynamics.py
+++ controls/rigid_dynamics.py
@@ -14,7 +14,7 @@ min_speed = 12.3 #24kt
 opti = asb.Opti()
 
 #define time, unknown duration
-final_time = opti.variable(init_guess=60, lower_bound=0)
+final_time = opti.variable(init_guess=12.0, lower_bound=0.0)
 time = np.linspace(0,final_time, 200)
 
 N = np.length(time) #number of time points
@@ -22,18 +22,18 @@ N = np.length(time) #number of time points
 #create dynamics instance, initial guesses for a roll
 dyn = asb.DynamicsRigidBody3DBodyEuler(
     mass_props=mass_properties,
-    x_e = opti.variable(init_guess=1000*np.linspace(0,1,N)),
-    y_e = opti.variable(init_guess=0*np.linspace(0,1,N)),
-    z_e = opti.variable(init_guess=-1000*np.ones(N)),
-    u_b = opti.variable(init_guess= 30*np.ones(N), upper_bound=0),
-    v_b = opti.variable(init_guess= 0*np.ones(N)),
-    w_b = opti.variable(init_guess= 0*np.ones(N)),
-    phi = opti.variable(init_guess= np.zeros(N)),
-    theta = opti.variable(init_guess= np.zeros(N)),
-    psi = opti.variable(init_guess = np.zeros(N)),
-    p = opti.variable(init_guess = np.concatenate((np.linspace(0,6,N//2), np.linspace(6,0,N//2)))),
-    q = opti.variable(init_guess = np.zeros(N)),
-    r = opti.variable(init_guess = np.zeros(N)))
+    x_e = opti.variable(init_guess=1000*np.linspace(0,1,N), n_vars=N),
+    y_e = opti.variable(init_guess=0*np.linspace(0,1,N), n_vars=N),
+    z_e = opti.variable(init_guess=-1000*np.ones(N), n_vars=N),
+    u_b = opti.variable(init_guess= 30*np.ones(N), n_vars=N, upper_bound=0),
+    v_b = opti.variable(init_guess= 0*np.ones(N), n_vars=N),
+    w_b = opti.variable(init_guess= 0*np.ones(N), n_vars=N),
+    phi = opti.variable(init_guess= np.zeros(N), n_vars=N),
+    theta = opti.variable(init_guess= np.zeros(N), n_vars=N),
+    psi = opti.variable(init_guess = np.zeros(N), n_vars=N),
+    p = opti.variable(init_guess = np.concatenate((np.linspace(0,6,N//2), np.linspace(6,0,N//2))), n_vars=N),
+    q = opti.variable(init_guess = np.zeros(N), n_vars=N),
+    r = opti.variable(init_guess = np.zeros(N), n_vars=N))
 
 #constrain initial state
 opti.subject_to([
@@ -48,17 +48,19 @@ opti.subject_to([
     dyn.r[0] == 0
     ])
 
+eps = 1e-1
 #constrain final state
 opti.subject_to([
-    dyn.u_b[-1] == 20,
-    dyn.v_b[-1] == 0,
-    dyn.w_b[-1] == 0,
-    dyn.phi[-1] == 2*np.pi,
-    dyn.theta[-1] == 0,
-    dyn.psi[-1] == 0,
-    dyn.p[-1] == 0,
-    dyn.q[-1] == 0,
-    dyn.r[-1] == 0
+    #dyn.u_b[-1] == 20,
+    #dyn.v_b[-1] == 0,
+    #dyn.w_b[-1] == 0,
+    dyn.phi[-1] < 2*np.pi+eps,
+    dyn.phi[-1] > 2*np.pi-eps,
+    #dyn.theta[-1] == 0,
+    #dyn.psi[-1] == 0,
+    #dyn.p[-1] == 0,
+    #dyn.q[-1] == 0,
+    #dyn.r[-1] == 0
     ])
 
 #add in forces
@@ -75,22 +77,28 @@ dyn.add_force(
 )
 
 #thrust variables
-thrust = opti.variable(init_guess= 1000*np.ones(N), lower_bound=0, upper_bound=2500)
+thrust = opti.variable(init_guess= 1000*np.ones(N), lower_bound=0, upper_bound=2500, n_vars=N)
 dyn.add_force(
     Fx=thrust,
     axes="body"
 )
 
 #roll moment
-roll = opti.variable(init_guess=np.concatenate((np.ones(N//2), -np.ones(N//2))))
+roll = opti.variable(init_guess=np.concatenate((10000*np.ones(N//2), -10000*np.ones(N//2))), n_vars=N, lower_bound=-10000, upper_bound=10000)
+#pitch moment
+pitch = opti.variable(init_guess=np.zeros(N), n_vars=N)
+#yaw moment
+yaw = opti.variable(init_guess=np.zeros(N), n_vars=N)
 dyn.add_moment(
     Mx=roll,
+    My=pitch,
+    Mz=yaw,
     axes="body"
 )
 
+
 dyn.constrain_derivatives(opti, time)
 opti.minimize(final_time)
-
 sol = opti.solve()
 try:
     dyn.substitute_solution(sol)
